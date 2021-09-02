5dd9fec61950f8e781da455640ffe66a84e9d9db
AleksandrShibanov <sashashibanov2016@gmail.com>
2021-09-02 16:25:14 +0300

    cosmetic changes

--- source/tree.js
+++ source/tree.js
@@ -6,26 +6,20 @@
  * @param {string|number} - высота елочки
  * @returns {(string|null)} - елочка|null
  */
-const tree = height => {
-  if (!Number.isInteger(+height)) {
+const tree = (height) => {
+  if (!Number.isInteger(+height) || height < 3) {
     return null;
   }
   
-  if (height < 3) {
-    return null;
-  }
-  
-  let res = '';
+  let result = '';
   for (let i = 0; i < height - 1; i++) {
     const stars = ('*').repeat(2 * i + 1);
     const spaces = (' ').repeat(height - 2 - i);
-    res += spaces + stars + spaces;
-    res += '\n';
+    result += spaces + stars + spaces + '\n';
   }
   
-  res += (' ').repeat(height - 2) + '|' + (' ').repeat(height - 2);
-  res += '\n';
+  result += (' ').repeat(height - 2) + '|' + (' ').repeat(height - 2) + '\n';
   
-  return res;
+  return result;
 }
 
